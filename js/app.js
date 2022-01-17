f47ab7ac9f1b1eb6d6144c99367db74e9931666f
Zhifeng He <homoumoumou@gmail.com>
2022-01-17 15:00:15 +1000

    hamburger-menu

--- js/app.js
+++ js/app.js
@@ -2,6 +2,9 @@ const filter_btns = document.querySelectorAll(".filter-btn");/* return all the  
 const skills_warp = document.querySelector(".skills");
 const skill_bars = document.querySelectorAll(".skill-progress");
 const footer_input = document.querySelector(".footer-input");
+const hamburger_menu = document.querySelector(".hamburger-menu");
+const navbar = document.querySelector("header nav");
+const links = document.querySelectorAll(".links a");
 
 footer_input.addEventListener("focus", () => {
   footer_input.classList.add("focus");
@@ -11,6 +14,23 @@ footer_input.addEventListener("blur", () => {
   if(footer_input.value != "") return;
   footer_input.classList.remove("focus");
 })
+
+function closeMenu() {
+    navbar.classList.remove("open");
+    document.body.classList.remove("stop-scrolling");
+  }
+  
+  hamburger_menu.addEventListener("click", ()=> {
+    if(!navbar.classList.contains("open")){
+      navbar.classList.add("open");
+      document.body.classList.add("stop-scrolling");
+    } else {
+      closeMenu();
+    }
+  })
+  
+  links.forEach((link) => link.addEventListener("click", () => closeMenu()));
+
 //inital isotope
 $(".grid").isotope({
     itemSelector: ".grid-item",
